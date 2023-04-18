e3886bab72e733766c05ac2879cf9de15acfbaa0
Abdullah Fahad <abdullah84f@gmail.com>
2023-04-18 22:39:41 +0300

    fix bugs, tiny changes

--- middleware/paramTeam.js
+++ middleware/paramTeam.js
@@ -1,10 +1,9 @@
 const { Team } = require("../models/team");
 const mongoose = require("mongoose");
 module.exports = async function (req, res, next) {
-  if (!mongoose.Types.ObjectId.isValid(req.params.teamId)) {
-    console.log(req.params);
+  if (!mongoose.Types.ObjectId.isValid(req.params.teamId))
     return res.status(400).send("invalid team ID.");
-  }
+
   const team = await Team.findById(req.params.teamId);
   if (!team)
     return res.status(404).send("The team with the given ID was not found.");
