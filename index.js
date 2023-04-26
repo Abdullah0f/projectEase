e80152cc59bdbad867b66a0d94830f1486ea03f8
Abdullah Fahad <abdullah84f@gmail.com>
2023-04-26 22:55:15 +0300

    bugfix
    bugfix
    
    bugfix
    
    bugfix
    
    bugfix

--- index.js
+++ index.js
@@ -6,14 +6,11 @@ require("joi").objectId = require("joi-objectid")(require("joi"));
 require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/prod")(app);
-console.log("Application Name: " + config.get("jwtPrivateKey"));
-console.log("Application Name: " + config.get("db"));
-//get jwtPrivateKey from node environment variable
-console.log("jwt" + process.env.jwtPrivateKey);
+
 let db = config.get("db");
 if (db.startsWith('"')) db = db.substring(1, db.length - 1);
 mongoose.connect(db).then(() => console.log("Connected to MongoDB..."));
-const server = app.listen(config.get("port"), () => {
-  winston.info("Server listening on port " + config.get("port"));
+const server = app.listen(config.get("port") || 3000, () => {
+  winston.info("Server listening on port " + config.get("port") || 3000);
 });
 module.exports = server;
