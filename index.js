b7bf2d9105e3022ae1e89f4a1a7586387f3e510c
Abdullah Fahad <abdullah84f@gmail.com>
2023-04-26 22:21:06 +0300

    bug fix

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ require("joi").objectId = require("joi-objectid")(require("joi"));
 require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/prod")(app);
-
+console.log("Application Name: " + config.get("jwtPrivateKey"));
+console.log("Application Name: " + config.get("db"));
+//get jwtPrivateKey from node environment variable
+console.log("jwt" + process.env.jwtPrivateKey);
 let db = config.get("db");
 if (db.startsWith('"')) db = db.substring(1, db.length - 1);
 mongoose.connect(db).then(() => console.log("Connected to MongoDB..."));
